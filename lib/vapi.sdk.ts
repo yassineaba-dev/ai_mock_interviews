4f5e9b9fc1f4efcaaec90c068bfc4012fc8a70ec
yassineaba-dev <yassineaba.dev@gmail.com>
2025-08-22 00:43:25 +0100

    first commit

--- lib/vapi.sdk.ts
+++ lib/vapi.sdk.ts
@@ -1,7 +1,7 @@
 // Server-only VAPI SDK instance
 import Vapi from "@vapi-ai/web";
 
-if (!process.env.VAPI_WEB_TOKEN) {
+if (process.env.NEXT_PUBLIC_VAPI_WEB_TOKEN!) {
   throw new Error("VAPI_WEB_TOKEN is missing in environment variables");
 }
 
